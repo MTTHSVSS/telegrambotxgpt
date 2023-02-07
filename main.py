7274c46066a11eafe88fb863d6defa029e20f3d6
Matthias Voss <118917115+MTTHSVSS@users.noreply.github.com>
2023-02-07 17:14:16 +0000

    -

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a Telegrambot using GPT3. What can I do for you?")
 
 async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    openai.api_key = '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'
+    openai.api_key = 'INSERT YOUR OPENAI API KEY HERE'
 
     response_text = openai.Completion.create(
         engine="text-davinci-003",
@@ -44,7 +44,7 @@ async def inline_caps(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.answer_inline_query(update.inline_query.id, results)
 
 if __name__ == '__main__':
-    application = ApplicationBuilder().token('5836563749:AAHt9P5W26lBk_g9BtEpNrWKMeSU0M1dGwA').build()
+    application = ApplicationBuilder().token('INSERT YOUR TELEGRAM BOT TOKEN HERE').build()
 
     start_handler = CommandHandler('start', start)
     echo_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), echo)
